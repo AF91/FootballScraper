45efe66e8a21d4bd3d25b8da4f30ead4c08937eb
AF91 <35491703+AF91@users.noreply.github.com>
2018-01-25 15:40:24 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ for name in names:
   # print name.text
   # print name.attrib['href']
   # store the link in the variable 'record' under the key 'link'
-  # record['link'] = name.attrib['href']
+  record['link'] = name.attrib['href']
   record['name'] = name.text.encode('ascii', 'ignore')
   print record
   scraperwiki.sqlite.save(unique_keys=['link'], data=record)
